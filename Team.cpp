df3b4956f1f0e217eebe75e99f9fc70710e4c1aa
Aidan Armas <aarmas3465@sdsu.edu>
2025-02-01 03:07:33 -0800

    Updated Team and Player methods

--- Team.cpp
+++ Team.cpp
@@ -29,12 +29,53 @@ string Team::getName() const { // METHOD FOR RETURNING TEAM NAME
 void Team::addPlayer(const Player &newPlayer) {
     if (roster.size() < 5) {
         roster.push_back(newPlayer);
-}
-    else {
+    } else {
         cout << "Failed Adding Player. " << name << " already has the 5 players" << endl;
     }
-
 }
+
+bool Team::removePlayer(const string &playerName) {
+        if (roster.empty()) {
+            cout << "Failed Removing Player. " << name << " already has 0 players" << endl;
+            return false;
+        }
+
+            for (auto it = roster.begin(); it != roster.end(); ++it) {
+                if (it->getName() == playerName) {
+                    roster.erase(it);
+                    cout << playerName << " has been removed from " << name << endl;
+                    return true;
+        }
+    }
+    cout << "Player " << playerName << " not found in " << name << endl;
+    return false;
+}
+
+void Team::displayRoster() const {
+    if (roster.empty()) {
+        cout << "Roster for " << name << " is empty" << endl;
+        return;
+    }
+
+    cout << name << " Team Roster: " << endl;
+    for (const auto &player : roster)  {
+        player.displayPlayer();
+    }
+}
+
+void Team::recordWin() {
+    wins++;
+}
+
+void Team::recordLoss() {
+    losses++;
+}
+
+void Team::displayRecord() {
+    cout << "Team: " << name << " | Wins: " << wins << " | Losses: " << losses << endl;
+}
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -6,19 +6,30 @@ using namespace std;
 int main() {
     Team team1("Uptime", "USA");
     Player player1("JJboyson","Entry","Poland");
-    Player player2("JJboyson","Entry","Poland");
-    Player player3("JJboyson","Entry","Poland");
-    Player player4("JJboyson","Entry","Poland");
-    Player player5("JJboyson","Entry","Poland");
-    Player player6("JJboyson","Entry","Poland");
+    Player player2("Godrod15","Support","Poland");
+    Player player3("Cav","IGL","Poland");
+    Player player4("Ragin","Support","Poland");
+    Player player5("Kappa","AWP","Poland");
 
-    player1.displayPlayer();
     team1.addPlayer(player1);
     team1.addPlayer(player2);
     team1.addPlayer(player3);
     team1.addPlayer(player4);
     team1.addPlayer(player5);
-    team1.addPlayer(player6);
+
+
+    team1.displayRoster();
+    cout << endl;
+
+    team1.recordWin();
+    team1.recordWin();
+    team1.recordLoss();
+    team1.recordLoss();
+    team1.recordLoss();
+    team1.recordLoss();
+
+    team1.displayRecord();
+
 
 
 
